4a2a899410ade9c7211ab7e811267b04ee3e8cd1
drake-smu <drakec@smu.edu>
2019-12-04 09:07:47 -0500

    new validation

--- exp/icassp_2019/cae.py
+++ exp/icassp_2019/cae.py
@@ -46,6 +46,8 @@ train = train / 255
 test = imread('data/kodak/kodim04.png')
 test = test / 255
 
+test2 = imread('data/kodak/kodim23.png')
+test2 = test2 / 255
 nb_param = sum(p.numel() for p in net.parameters() if p.requires_grad)
 
 print("QCAE & CAE Color images - Titouan Parcollet - LIA, ORKIS")
@@ -63,6 +65,9 @@ if MODEL == 'QCAE':
     test = np.transpose(test, (2,0,1))
     test = np.reshape(test, (1, test.shape[0], test.shape[1], test.shape[2]))
 
+    test2 = np.pad(test2, pad_width=npad, mode='constant', constant_values=0)
+    test2 = np.transpose(test2, (2,0,1))
+    test2 = np.reshape(test2, (1, test2.shape[0], test2.shape[1], test2.shape[2]))
 else:
     train = np.transpose(train, (2,0,1))
     train = np.reshape(train, (1, train.shape[0], train.shape[1], train.shape[2]))
@@ -70,12 +75,17 @@ else:
     test  = np.transpose(test, (2,0,1))
     test  = np.reshape(test, (1, test.shape[0], test.shape[1], test.shape[2]))
 
+    test2  = np.transpose(test2, (2,0,1))
+    test2  = np.reshape(test2, (1, test2.shape[0], test2.shape[1], test2.shape[2]))
+
 if CUDA:
     train = torch.from_numpy(train).float().CUDA()
     test  = torch.from_numpy(test).float().CUDA()
+    test2  = torch.from_numpy(test2).float().CUDA()
 else:
     train = torch.from_numpy(train).float()
     test  = torch.from_numpy(test).float()
+    test2  = torch.from_numpy(test2).float()
 
 for epoch in range(NUM_EPOCHS):
 
@@ -100,4 +110,19 @@ for epoch in range(NUM_EPOCHS):
             out = np.reshape(out, (out.shape[1], out.shape[2], out.shape[3]))
 
 
-        imwrite("out/save_image"+str(epoch)+".png", out)
+        imwrite("out/save_image_"+str(MODEL)+"_"+str(epoch)+".png", out)
+
+validate = net(test2)
+val_loss = criterion(validate,test2)
+val_loss.backward()
+val  = validate.cpu().data.numpy()
+
+if MODEL == 'QCAE':
+    val = np.transpose(val, (0,2,3,1))[:,:,:,1:]
+    val = np.reshape(val, (val.shape[1], val.shape[2], val.shape[3]))
+else:
+    val = np.transpose(val, (0,2,3,1))
+    val = np.reshape(val, (val.shape[1],val.shape[2],val.shape[3]))
+print("#"*50+"\nFinal Eval",str(val_loss.cpu().item()));
+imwrite("out/test_out_"+str(MODEL)+".png",val)
+imwrite("out/test_in_"+str(MODEL)+".png",imread('data/kodak/kodim23.png'))
